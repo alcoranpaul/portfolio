2acb4d28a270c8d59154bd8200fb872caa4de07f
Paul Adrian <paulreyes74@yahoo.com>
2023-05-30 16:20:37 -0500

    init

---
+++ src/store/root-reducer.js
@@ -0,0 +1,14 @@
+/*
+ * File: root-reducert.js
+ * Project: portfolio
+ * File Created: Tuesday, 30th May 2023 4:20:24 pm
+ * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
+ * GitHub: https://github.com/alcoranpaul
+ * -----
+ * Last Modified: Tuesday, 30th May 2023 4:20:24 pm
+ * Modified By: PR (paulreyes74@yahoo.com>)
+ * -----
+ * -----
+ * Description:
+ */
+
---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+/*
+ * File: store.js
+ * Project: portfolio
+ * File Created: Tuesday, 30th May 2023 4:19:34 pm
+ * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
+ * GitHub: https://github.com/alcoranpaul
+ * -----
+ * Last Modified: Tuesday, 30th May 2023 4:20:11 pm
+ * Modified By: PR (paulreyes74@yahoo.com>)
+ * -----
+ * -----
+ * Description:
+ */
+
+import { compose, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
