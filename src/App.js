bc695536af61f73e7c3d62c8197a727f34026f77
Paul Adrian <paulreyes74@yahoo.com>
2023-05-30 16:41:56 -0500

    added some dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+
+const projectTemplate = {
+  project1: {
+    name: 'Project Name 1',
+    description: 'Project Description 1',
+    image: 'Project Image 1',
+  },
+  project2: {
+    name: 'Project Name 2',
+    description: 'Project Description 2',
+    image: 'Project Image 2',
+  }
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {Object.values(projectTemplate).map((project, index) => (
+        <div key={index}>
+          <h2>{project.name}</h2>
+          <p>{project.description}</p>
+          <img src={project.image} alt={project.name} />
+        </div>
+      ))}
+    </>
   );
 }
 
--- src/store/category/category.reducer.js
+++ src/store/category/category.reducer.js
@@ -5,21 +5,22 @@
  * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
  * GitHub: https://github.com/alcoranpaul
  * -----
- * Last Modified: Tuesday, 30th May 2023 4:27:06 pm
+ * Last Modified: Tuesday, 30th May 2023 4:40:20 pm
  * Modified By: PR (paulreyes74@yahoo.com>)
  * -----
  * -----
  * Description:
  */
 
+import { CATEGORY_ACTION_TYPE } from "./category.types";
 const INITIAL_STATE = {
     categories: []
 }
 
-const categoryReducer = (state = {}, action = {}) => {
+export const categoryReducer = (state = {}, action = {}) => {
     const { type, payload } = action;
     switch (type) {
-        case CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP:
+        case CATEGORY_ACTION_TYPE.SET_CATEGORIES_MAP:
             return {
                 ...state, //Spread the previous state 
                 categoriesMap: payload // Update currentUser and overwrite existing value with payload
