4e9cdd70224984685dbbcbf18cf5a07e6dbfe86d
Paul Adrian <paulreyes74@yahoo.com>
2023-06-09 17:13:01 -0500

    Added Arrow SVG

--- src/components/projectPreview/projectPreview.component.jsx
+++ src/components/projectPreview/projectPreview.component.jsx
@@ -5,7 +5,7 @@
  * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
  * GitHub: https://github.com/alcoranpaul
  * -----
- * Last Modified: Friday, 9th June 2023 4:22:02 pm
+ * Last Modified: Friday, 9th June 2023 5:12:35 pm
  * Modified By: PR (paulreyes74@yahoo.com>)
  * -----
  * -----
@@ -13,10 +13,14 @@
  */
 
 import { Container, Row, Col } from 'react-bootstrap';
+import Arrow from '../../data/home/icons/arrowIcon';
 import './projectPreview.styles.scss';
 const ProjectPreview = ({ project }) => {
     return (
         <Container className='main-content'>
+            <div className='leftArrow'>
+                <Arrow />
+            </div>
             <Row className='projects-container'>
                 <Col className='project-one'>1</Col>
                 <Col className='project-two'>2</Col>
---
+++ src/data/home/icons/arrowIcon.jsx
@@ -0,0 +1,35 @@
+/*
+ * File: arrowIcon
+ * Project: portfolio
+ * File Created: Friday, 9th June 2023 5:12:04 pm
+ * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
+ * GitHub: https://github.com/alcoranpaul
+ * -----
+ * Last Modified: Friday, 9th June 2023 5:12:41 pm
+ * Modified By: PR (paulreyes74@yahoo.com>)
+ * -----
+ * -----
+ * Description:
+ */
+
+function Arrow() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="127.984"
+            height="127.984"
+            viewBox="0 0 95.988 95.988"
+        >
+            <path
+                fill="none"
+                stroke="#000"
+                strokeLinecap="square"
+                strokeLinejoin="bevel"
+                strokeWidth="4.999"
+                d="M67.398 9.415l-37.89 38.58L66.71 85.768"
+            ></path>
+        </svg>
+    );
+}
+
+export default Arrow;
