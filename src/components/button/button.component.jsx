7f1691c69ff3a7f766cdcdabf34da6ae9f8f246c
Paul Adrian <paulreyes74@yahoo.com>
2023-06-12 22:23:16 -0500

    added link functionality

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -5,7 +5,7 @@
  * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
  * GitHub: https://github.com/alcoranpaul
  * -----
- * Last Modified: Monday, 12th June 2023 9:47:03 pm
+ * Last Modified: Monday, 12th June 2023 10:23:04 pm
  * Modified By: PR (paulreyes74@yahoo.com>)
  * -----
  * -----
@@ -16,9 +16,9 @@ import { Button, ButtonIcon, ButtonText } from "./button.styles";
 import { BUTTON_TYPES } from "./buttonTypes";
 
 
-const CustomButton = ({ type, disabled, children }) => {
+const CustomButton = ({ type, disabled, children, linkTo }) => {
     return (
-        <Button type={type} disabled={disabled}>
+        <Button type={type} disabled={disabled} onClick={linkTo}>
             <ButtonIcon>{children}</ButtonIcon>
             <ButtonText>
                 {type === BUTTON_TYPES.DEMO ? 'Demo' : 'Github'}
