92b5885040fea8832b230efa5ca1600461477d9d
Paul Adrian <paulreyes74@yahoo.com>
2023-06-14 00:17:32 -0500

    Change to Unfold Icon

--- src/data/components/dropdown/arrowUpDown.jsx
+++ src/data/components/dropdown/arrowUpDown.jsx
@@ -5,7 +5,7 @@
  * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
  * GitHub: https://github.com/alcoranpaul
  * -----
- * Last Modified: Wednesday, 14th June 2023 12:13:09 am
+ * Last Modified: Wednesday, 14th June 2023 12:17:13 am
  * Modified By: PR (paulreyes74@yahoo.com>)
  * -----
  * -----
@@ -18,12 +18,24 @@ function ArrowUpDown() {
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
-            fill="var(--color-text)"
             version="1.1"
-            viewBox="0 0 34.695 34.695"
-            xmlSpace="preserve"
+            fill="var(--color-text)"
+            viewBox="0 0 400 400"
         >
-            <path d="M28.708 18.979a.503.503 0 01-.029.517L18.395 34.479a.5.5 0 01-.397.217h-.015a.505.505 0 01-.396-.194L6.015 19.518a.497.497 0 01.396-.804h21.854c.187-.003.356.1.443.265zM6.41 15.983h21.875a.5.5 0 00.308-.896L17.088.194a.515.515 0 00-.41-.194.498.498 0 00-.398.218L5.998 15.2a.5.5 0 00.412.783z"></path>
+            <g transform="translate(0 -652.362)">
+                <path
+                    fill="var(--color-text)"
+                    fillOpacity="1"
+                    stroke="none"
+                    strokeDasharray="none"
+                    strokeDashoffset="0"
+                    strokeMiterlimit="4"
+                    strokeOpacity="1"
+                    strokeWidth="25"
+                    d="M200 1052.912L341.422 911.49l-17.678-17.678L200 1017.557 76.256 893.812 58.578 911.49zm123.744-242l17.678-17.678L200 651.812 58.578 793.234l17.678 17.678L200 687.168z"
+                    opacity="1"
+                ></path>
+            </g>
         </svg>
     );
 }
