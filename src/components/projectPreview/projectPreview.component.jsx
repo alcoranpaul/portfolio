e9aecdeac25947c346fb8b2ebf3616be56c595b4
Paul Adrian <paulreyes74@yahoo.com>
2023-06-15 14:50:54 -0500

    Deleted some spaces

--- src/components/projectPreview/projectPreview.component.jsx
+++ src/components/projectPreview/projectPreview.component.jsx
@@ -5,7 +5,7 @@
  * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
  * GitHub: https://github.com/alcoranpaul
  * -----
- * Last Modified: Wednesday, 14th June 2023 1:43:33 am
+ * Last Modified: Thursday, 15th June 2023 12:20:56 am
  * Modified By: PR (paulreyes74@yahoo.com>)
  * -----
  * -----
