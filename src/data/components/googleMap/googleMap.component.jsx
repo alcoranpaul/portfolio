6db41255519372506b7f60bdf498c11397fc2f9a
Paul Adrian <paulreyes74@yahoo.com>
2023-06-19 16:15:19 -0500

    added Google Map

---
+++ src/data/components/googleMap/googleMap.component.jsx
@@ -0,0 +1,45 @@
+/*
+ * File: googleMap.component.jsx
+ * Project: portfolio
+ * File Created: Monday, 19th June 2023 3:53:07 pm
+ * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
+ * GitHub: https://github.com/alcoranpaul
+ * -----
+ * Last Modified: Monday, 19th June 2023 4:15:08 pm
+ * Modified By: PR (paulreyes74@yahoo.com>)
+ * -----
+ * -----
+ * Description:
+ */
+
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+
+export default function SimpleMap() {
+    const defaultProps = {
+        center: {
+            lat: 49.9,
+            lng: -97.1411110
+        },
+        zoom: 11
+    };
+
+    return (
+
+        <div style={{ height: '100%', width: '100%', paddingRight: '10px' }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+                defaultCenter={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+            >
+                <AnyReactComponent
+                    lat={49.9}
+                    lng={-97.4}
+                    text="My Marker"
+                />
+            </GoogleMapReact>
+        </div>
+    );
+}
--- src/routes/contact/contact.component.jsx
+++ src/routes/contact/contact.component.jsx
@@ -5,7 +5,7 @@
  * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
  * GitHub: https://github.com/alcoranpaul
  * -----
- * Last Modified: Monday, 19th June 2023 3:25:06 pm
+ * Last Modified: Monday, 19th June 2023 3:56:42 pm
  * Modified By: PR (paulreyes74@yahoo.com>)
  * -----
  * -----
@@ -13,21 +13,58 @@
  */
 
 import Platforms from "../../components/platforms/platforms.component";
+import styled from 'styled-components';
+import SimpleMap from "../../data/components/googleMap/googleMap.component";
 
 const Contact = () => {
     return (
-        <div>
-            <div className="platform-container"
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}>
+        <Container>
+            <MapAndEmailContainer>
+                <Box1>
+                    <SimpleMap />
+                </Box1>
+                <Box2 />
+            </MapAndEmailContainer>
+            <PlatformContainer>
                 <Platforms />
-            </div>
-        </div>
+            </PlatformContainer>
+        </Container>
     );
 }
 
 export default Contact;
+
+const Container = styled.div`
+
+    flex-direction: column;
+    align-items: center;
+`;
+
+const MapAndEmailContainer = styled.div`
+    width: 100%;
+    height: 600px;
+    background-color: var(--color-primary);
+    margin: 20px 0px;
+    padding: 20px;
+    display: flex;
+`;
+
+const Box1 = styled.div`
+    width: 75%;
+    height: 100%;
+    background-color: #f0f0f0;
+`;
+
+const Box2 = styled.div`
+    width: 25%;
+    height: 100%;
+    background-color: #c0c0c0;
+`;
+
+const PlatformContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 20px;
+`;
