9ed5fcf0f24994d440f7c3a471521405e5c0b7ac
Paul Adrian <paulreyes74@yahoo.com>
2023-06-20 18:50:25 -0500

    removed "/"

--- src/App.js
+++ src/App.js
@@ -29,12 +29,12 @@ function App() {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/skills' element={<Skills />} />
-        <Route path='/work' element={<Work />} />
-        <Route path='/education' element={<Education />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/about' element={<About />} />
+        <Route path='projects' element={<Projects />} />
+        <Route path='skills' element={<Skills />} />
+        <Route path='work' element={<Work />} />
+        <Route path='education' element={<Education />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='about' element={<About />} />
       </Route>
     </Routes>
   );
