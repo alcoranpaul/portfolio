476d5797f0cf82a229066edc1db007ca9fb5da39
Paul Adrian <paulreyes74@yahoo.com>
2023-06-20 17:19:06 -0500

    addeed Bold and Link tags

--- src/components/sectionCard/sectionCard.component.jsx
+++ src/components/sectionCard/sectionCard.component.jsx
@@ -5,27 +5,52 @@
  * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
  * GitHub: https://github.com/alcoranpaul
  * -----
- * Last Modified: Tuesday, 20th June 2023 3:34:51 pm
+ * Last Modified: Tuesday, 20th June 2023 5:17:25 pm
  * Modified By: PR (paulreyes74@yahoo.com>)
  * -----
  * -----
  * Description:
  */
 
-import { Container, Title, Body } from "./sectionCard.styles";
-
-
+import React from 'react';
+import { Container, Title, Body } from './sectionCard.styles';
+import { Link } from 'react-router-dom';
 
 const SectionCard = ({ title, body }) => {
-    const bodyLines = body.split('\n'); // Split the body text by newline character
-
+    const bodyLines = body.split('\n');
 
     return (
         <Container>
-            <Title>{title}</Title>
+            <Title id={title}>{title}</Title>
             <Body>
                 {bodyLines.map((line, index) => (
-                    <p key={index}>{line}</p> // Create a separate paragraph element for each line
+                    <p key={index}>
+                        {line.split('<b>').map((text, i) => {
+                            if (i % 2 === 1) {
+                                // Text between <b> tags
+                                return <span className="bolded" key={i}>{text}</span>;
+                            } else {
+                                // Normal text
+                                return text.split('<link>').map((segment, j) => {
+                                    if (j % 2 === 1) {
+                                        // Text between <link> tags
+                                        const linkToWord = segment.trim(); // Trim the linkToWord to remove leading/trailing spaces
+                                        const toValue = linkToWord.match(/\((.*?)\)/)[1]; // Extract value between parentheses
+                                        const textWithoutParentheses = linkToWord.replace(/\(.*?\)/, ''); // Remove parentheses from link text
+                                        console.log(toValue); // Log the extracted 'to' value
+                                        return (
+                                            <Link className="linkTo" to={toValue} key={j}>
+                                                {textWithoutParentheses}
+                                            </Link>
+                                        );
+                                    } else {
+                                        // Normal text
+                                        return segment;
+                                    }
+                                });
+                            }
+                        })}
+                    </p>
                 ))}
             </Body>
         </Container>
@@ -33,3 +58,4 @@ const SectionCard = ({ title, body }) => {
 };
 
 export default SectionCard;
+
--- src/components/sectionCard/sectionCard.styles.jsx
+++ src/components/sectionCard/sectionCard.styles.jsx
@@ -5,7 +5,7 @@
  * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
  * GitHub: https://github.com/alcoranpaul
  * -----
- * Last Modified: Tuesday, 20th June 2023 3:37:49 pm
+ * Last Modified: Tuesday, 20th June 2023 4:10:32 pm
  * Modified By: PR (paulreyes74@yahoo.com>)
  * -----
  * -----
@@ -36,10 +36,32 @@ export const Title = styled.h1`
 export const Body = styled.div`
   ${'' /* background-color: blue; */}
   font-size: 25px;
-  line-height: 2;
+  line-height: 1.9;
   letter-spacing: 1px;
-  
+  color: var(--color-accent);
   text-align: left;
   margin-bottom: 10px;
   width: 80%;
+
+  .bolded {
+    font-weight: bold;
+    font-size: 30px;
+    color: var(--color-text);
+  }
+
+  .linkTo {
+    color: var(--color-text);
+    text-decoration: underscore;
+    font-weight: bold;
+    font-size: 30px;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      color: var(--color-linkHover);
+    }
+  }
+
+
+  
+
 `;
