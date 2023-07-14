148eb37a4cd8a5361cac99074b165dd21fe017b3
Paul Adrian <paulreyes74@yahoo.com>
2023-07-14 00:29:16 -0500

    another FIX finally

--- src/store/date/date.saga.js
+++ src/store/date/date.saga.js
@@ -5,7 +5,7 @@
  * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
  * GitHub: https://github.com/alcoranpaul
  * -----
- * Last Modified: Thursday, 13th July 2023 11:35:59 pm
+ * Last Modified: Friday, 14th July 2023 12:25:22 am
  * Modified By: PR (paulreyes74@yahoo.com>)
  * -----
  * -----
@@ -64,10 +64,12 @@ export function* checkDateProcess() {
         yield put(checkDate())
         const localDate = yield select(selectSavedDate)
 
-        if (!localDate || !(localDate instanceof Date)) {
+        if (!localDate) {
+
+            const currentDate = new Date();
             yield call(settingNewDateProcess, currentDate)
             return {
-                currentDate: new Date(),
+                currentDate,
                 isSavedDateOutsideThreshold: false
             };
         }
